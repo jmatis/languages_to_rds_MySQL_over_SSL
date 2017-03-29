5199e4e1ca4cc5f9d963827a9c6e0e5b16dcef88
jmatis <jan.matis@gmail.com>
2017-03-29 21:02:52 +0100

    Update testMySQLSSL.py

--- python/testMySQLSSL.py
+++ python/testMySQLSSL.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
+import MySQLdb
 user=""
 passwd=""
 host=""
 db=""
-ssl={‘cert’: ‘/root/mysql-ssl/client-cert.pem’}
+ssl={'ca': '/root/rds-combined-ca-bundle.pem', 'verify_cert': True}
 db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db, ssl=ssl)
 cursor = db.cursor()
-cursor.execute(‘SHOW STATUS like “Ssl_cipher”‘)
+cursor.execute('SHOW STATUS like "Ssl_cipher"')
 print cursor.fetchone()
 db.close()
