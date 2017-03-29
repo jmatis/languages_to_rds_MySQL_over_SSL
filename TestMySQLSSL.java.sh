43d1742402b6faf74c3cb5184896f0879180a8c1
jmatis <jan.matis@gmail.com>
2017-03-29 19:59:26 +0100

    Update TestMySQLSSL.java.sh

--- TestMySQLSSL.java.sh
+++ TestMySQLSSL.java.sh
@@ -3,6 +3,9 @@
 # first download and unpack mysql connector (if not already downloaded)
 if [ ! -f "mysql-connector-java-5.1.41-bin.jar" ]; then
   wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.41.tar.gz
+  tar xf mysql-connector-java-5.1.41.tar.gz mysql-connector-java-5.1.41/mysql-connector-java-5.1.41-bin.jar
+  mv mysql-connector-java-5.1.41/mysql-connector-java-5.1.41-bin.jar ./
+  rmdir mysql-connector-java-5.1.41
 fi
 
 # now compile our java code (i have tested this in java8 
