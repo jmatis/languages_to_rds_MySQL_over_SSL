66c3167ede0cf9f07924e5235169f0a7797993fc
jmatis <jan.matis@gmail.com>
2017-03-29 21:42:04 +0100

    Update TestMySQLSSL.java
    updated to actually print ssl_cipher

--- java/TestMySQLSSL.java
+++ java/TestMySQLSSL.java
@@ -36,8 +36,46 @@ public class TestMySQLSSL
                 {
                         e.printStackTrace();
                 }
+                Statement stmt = null;
+                ResultSet rs = null;
+
+                try {
+                        stmt = con.createStatement();
+                        if (stmt.execute("SHOW STATUS like 'Ssl_cipher'")) {
+                                rs = stmt.getResultSet();
+                                //System.out.println(rs.getString(2));
+                                ResultSetMetaData rsmd = rs.getMetaData();
+                                int columnsNumber = rsmd.getColumnCount();
+                                while (rs.next()) {
+                                        for (int i = 1; i <= columnsNumber; i++) {
+                                                if (i > 1) System.out.print(",  ");
+                                                String columnValue = rs.getString(i);
+                                                System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                                        }
+                                        System.out.println("");
+                                }
+                        }
+                }
+                catch (SQLException e){
+                        System.err.println("SQLException: " + e.getMessage());
+                        System.err.println("SQLState: " + e.getSQLState());
+                        System.err.println("VendorError: " + e.getErrorCode());
+                }
                 finally
                 {
+                        if (rs != null) {
+                                try {
+                                        rs.close();
+                                } catch (SQLException sqlEx) { } // ignore
+                                rs = null;
+                        }
+
+                        if (stmt != null) {
+                                try {
+                                        stmt.close();
+                                } catch (SQLException sqlEx) { } // ignore
+                                stmt = null;
+                        }
                         if (con != null)
                         {
                                 try
